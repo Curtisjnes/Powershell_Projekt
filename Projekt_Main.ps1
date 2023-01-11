8b7bc31e619ccf9ccd7be3f9d9c9fc6ea2d4582b
Curtisjnes <curtisclajones@gmail.com>
2023-01-11 15:51:29 +0100

    erster Commit

---
+++ Projekt_Main.ps1
@@ -0,0 +1,38 @@
+﻿
+function menue (){
+	$auswahl = 
+	@(
+		'Beenden',
+	 	'Benutzer anlegen',
+		'Benutzer löschen',
+	 	'Benutzer bearbeiten',
+		'Mehrere Benutzer anlegen',
+		'Mehrere Benutzer Löschen',
+		'Dienste ausgeben',
+		'Dienst starten',
+		'Dienstanhalten',
+        'Netzwerktest'
+	)
+	
+	$s = 0
+	
+	foreach ( $node in $auswahl )
+	{	
+   		Write-Host "[$s]: [$node]"
+		$s = $s+1
+	}
+
+    
+}
+
+function Get-Input{
+        param($input)
+    
+}
+
+
+menue
+$input = Read-Host "Eingabe tätigen "
+
+    
+
