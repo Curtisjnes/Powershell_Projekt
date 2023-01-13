357260df9cb15ee11b88c7ab4b98e0e1a70560d5
Curtisjnes <curtisclajones@gmail.com>
2023-01-13 14:06:04 +0100

    functioning menue selection and i implemented some functions

--- Projekt_Main.ps1
+++ Projekt_Main.ps1
@@ -1,4 +1,61 @@
 ﻿
+
+function Get-Input([String] $eingabe){
+      
+      switch($eingabe)
+      {
+            "0" {Exit 1}
+            "1" {Benutzer-Anlegen}
+            "2" {Benutzer-Loeschen}
+            "3" {Benutzer-Bearbeiten}
+            "4" {Mehrere-Benutzer-Anlegen}
+            "5" {Mehrere-Benutzer-Loeschen}
+            "6" {Dienste-Ausgeben}
+            "7" {Dienst-Starten}
+            "8" {Dienst-Anhalten}
+            "9" {Netzwerktest}
+      }
+
+    
+}
+
+function Benutzer-Anlegen{
+
+    $passwort = Read-Host "Passwort festlegen " -AsSecureString
+    $Name = Read-Host "Einmal bitte den Vornamen des Users eingeben "
+    $Nachname = Read-Host "Einmal bitte den Nachnamen des Users eingeben "
+    
+    Write-Host "New-LocalUser $Name -Password [$passwort] -FullName [$Name $Nachname] -Description CompleteVisibility"
+     
+
+}
+
+function Benutzer-Loeschen(){
+        
+       Get-LocalUser | Select *
+       $benutzer = Read-Host "Welchen Benutzer wollen Sie löschen? (Vollen Namen eingeben) "
+       Remove-LocalUser -Name $benutzer
+
+
+}
+
+function Benutzer-Bearbeiten{
+    
+    Get-LocalUser | Select *
+    $benutzer = Read-Host "Welchen Benutzer wollen Sie löschen? (Vollen Namen eingeben) "
+    Edit-LocalUser -Name $benutzer
+
+}
+
+function Mehrere-Benutzer-Anlegen{
+
+    $pfad = Read-Host "Bitte den Pfad der Datei hier reinkopieren "
+
+
+}
+
+
+
 function menue (){
 	$auswahl = 
 	@(
@@ -10,7 +67,7 @@ function menue (){
 		'Mehrere Benutzer Löschen',
 		'Dienste ausgeben',
 		'Dienst starten',
-		'Dienstanhalten',
+		'Dienst anhalten',
         'Netzwerktest'
 	)
 	
@@ -25,21 +82,11 @@ function menue (){
     
 }
 
-function Get-Input{
-        param($input)
-      
-      if($input -eq "3"){
-        Write-Host "hello"
-      }else{
-        Write-Host "HALLO"
-      }
 
-    
-}
 
 menue
-$input = Read-Host "Eingabe tätigen"
-Get-Input($input)
-
+[String] $eingabe = Read-Host "Eingabe tätigen"
+Write-Host $eingabe.GetType().Name
+Get-Input -eingabe $eingabe 
     
 
